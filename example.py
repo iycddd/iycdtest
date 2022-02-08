54db2af24a2e32ad1206719c8c0b90753802fe24
iycddd <98306965+iycddd@users.noreply.github.com>
2022-02-08 16:58:49 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
 from st_aggrid import AgGrid
 import pandas as pd
 import streamlit as st
+import chime
 
 df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
 AgGrid(df)
+
+
+if st.button(""):
+  chime.success()
